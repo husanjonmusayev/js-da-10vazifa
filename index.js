587a0464a7f7edfd704c838c61b16b6331030f93
husanjonmusayev <husanjonm279@gmail.com>
2023-12-15 19:36:52 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,54 @@
+// Vazifa
+// 1-masala
+
+// let err1 = [1,2,3,];
+
+// let err2 = [4,5,6,];
+
+// let err3 = [7,8,9,];
+
+// let err4 = err3.concat(err2,err1);
+// console.log(err4);
+
+// 2-masala
+// let array = [1,2,15,7,25,30]
+// let sum = 1;
+// array.forEach((valeo, index) => {
+//     if(valeo % 5 ==0 && index % 2 ==0){
+//         sum = index**2
+//         return sum
+//     }
+    
+// });
+// console.log(sum);
+
+// 3-masala
+// let array = [1,2,-15,7,-25,30]
+// let man = array.find((valeo) =>{
+//     return valeo < 0
+// });
+
+// console.log(man)
+
+// 4-masala
+//let array = [1,2,15,7,25,30]
+// let sum = 0;
+// array.forEach((valeo, index) => {
+//     if(valeo % 3 ==0 && index % 2 ==1){
+//         sum = ++
+//         return sum
+//     }
+    
+// });
+// console.log(sum);
+
+// 5-masala
+// let array = [1,2,-15,7,-25,30]
+// console.log(array.includes());
+
+// 6-masala
+// let array = [1,2,15,7,25,30]
+// let sort = array.sort(function(a,b){
+//     return b-a
+// });
+// console.log(sort);
